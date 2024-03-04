9c71539307ae88b9f34ddf64f8239f458afdb208
mzain787 <zain26062001@gmail.com>
2024-03-04 21:16:00 +0500

    vtube-1

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongo DB Connection Error", error);
+  });
+
+import express from "express";
+const app = express();
+
+// iife
+// ;import { DB_NAME } from "./constants";
+// (async () => {
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`,)
+//         app.on('error',()=>{
+//             console.log('Error',error)
+//             throw err
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`Server is running on port ${process.env.PORT}`)
+//         })
+//     }
+//     catch(error){
+//         console.log('Error',error)
+//         throw err
+//     }
+// })()
